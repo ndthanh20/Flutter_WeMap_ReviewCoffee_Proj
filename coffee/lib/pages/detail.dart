ab0cb701a7afd236b251f6546a5c49482ce22bba
NDM2108 <63856585+NDM2108@users.noreply.github.com>
2021-06-11 17:18:29 +0700

    thêm icon map

--- coffee/lib/pages/detail.dart
+++ coffee/lib/pages/detail.dart
@@ -4,7 +4,13 @@ class Detail extends StatefulWidget {
   final String image;
   final String name;
   final String address;
-  const Detail({Key? key, required this.image, required this.name, required this.address}) : super(key: key);
+
+  const Detail(
+      {Key? key,
+      required this.image,
+      required this.name,
+      required this.address})
+      : super(key: key);
 
   @override
   _DetailState createState() => _DetailState();
@@ -24,21 +30,29 @@ class _DetailState extends State<Detail> {
             height: MediaQuery.of(context).size.height * 0.4,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage(widget.image),
-                    fit: BoxFit.cover)),
+                image: DecorationImage(
+                    image: AssetImage(widget.image), fit: BoxFit.cover)),
           ),
           Container(
-            margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
-            child: Text(widget.address,
-            style: TextStyle(
-              fontFamily: 'SourceSansPro',
-              fontSize: 22,
-              color: Colors.brown,
-            ),),
+            margin: EdgeInsets.fromLTRB(10, 50, 0, 0),
+            child: Row(
+              children: [
+                IconButton(
+                    onPressed: () {print(1);},
+                    icon: Icon(Icons.fmd_good)),
+                Text(
+                  widget.address,
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 22,
+                    color: Colors.brown,
+                  ),
+                ),
+              ],
+            ),
           )
         ],
       ),
     );
   }
 }
-
